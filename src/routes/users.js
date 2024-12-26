540b75c9d62bddba1fc5f4757258820e5c77c2c7
awenkoo <186723021+awenkoo@users.noreply.github.com>
2024-12-26 14:36:17 +0200

    refactor: i forgor

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,11 @@
 import express from 'express';
 
-import {
-  deleteUser,
-  getAllUsers,
-  getUserById,
-  updateUser,
-} from '#controllers/users.js';
+import { getAllUsers, getUserById, updateUser } from '#controllers/users.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
 router.patch('/:id', updateUser);
-router.delete('/:id', deleteUser);
 
 export default router;
--- src/routes/vote.js
+++ src/routes/vote.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
-import { getVotesByUserId, updateVotes } from '#controllers/vote.js';
+import { updateVotes } from '#controllers/vote.js';
 
 const router = express.Router();
 
-router.get('/:id', getVotesByUserId);
 router.patch('/:id', updateVotes);
 
 export default router;
